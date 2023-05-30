c8d0b8081e7f211d0d910e0e3bacfcb8057de854
Pedram Davoodi <pedramdavoodi1376@gmail.com>
2023-05-30 21:24:14 +0430

    Add multi update function

--- src/Traits/Updateable.php
+++ src/Traits/Updateable.php
@@ -1,5 +1,34 @@
 <?php
 
+namespace pedram\multiUpdate\Traits;
+
+use Illuminate\Support\Facades\DB;
+
 Trait Updateable{
 
+    /**
+     * Perform a multi-row update on the model's table based on the given parameters.
+     *
+     * @param array $params An associative array where the keys are the names of the columns to update,
+     *                      and the values are nested associative arrays where the keys are the new values to set,
+     *                      and the values are the conditions to match for each new value.
+     * @return int|bool The number of rows affected by the update query, or false if an error occurred.
+     */
+    public static function multiUpdate($params)
+    {
+        try {
+            $table = (new static)->getTable();
+            $query = "UPDATE $table SET";
+            foreach ($params as $field => $param) {
+                $query .= " $field = CASE";
+                foreach ($param as $value => $condition) {
+                    $query .= " WHEN $condition THEN '$value' ";
+                }
+                $query .= " ELSE $field END,";
+            }
+            return DB::update(rtrim($query, ','));
+        } catch (\Throwable $exception) {
+            return false;
+        }
+    }
 }
