53091895fef9fc3ec72a4fe9e0d9bd14b64f51aa
Pedram Davoodi <pedramdavoodi1376@gmail.com>
2023-05-30 23:09:08 +0430

    add where to query builder

--- src/Traits/MultiUpdateable.php
+++ src/Traits/MultiUpdateable.php
@@ -12,9 +12,10 @@ Trait MultiUpdateable{
      * @param array $params An associative array where the keys are the names of the columns to update,
      *                      and the values are nested associative arrays where the keys are the new values to set,
      *                      and the values are the conditions to match for each new value.
+     * @param string ...$conditions A variable-length argument list of conditions to apply to the update statement Each condition should be a string in the format of a valid SQL WHERE clause.
      * @return int|bool The number of rows affected by the update query, or false if an error occurred.
      */
-    public static function updateMultipleRows($params)
+    public static function updateMultipleRows($params , string ...$conditions)
     {
         try {
             $table = (new static)->getTable();
@@ -26,7 +27,14 @@ Trait MultiUpdateable{
                 }
                 $query .= " ELSE $field END,";
             }
-            return DB::update(rtrim($query, ','));
+            $query = rtrim($query, ',');
+
+            $where = '';
+            foreach ($conditions as $condition)
+                $where .= $condition .' AND ';
+            $where = rtrim($where, ' AND ');
+
+            return DB::update(empty($where) ? $query : $query . " WHERE " . $where);
         } catch (\Throwable $exception) {
             return false;
         }
